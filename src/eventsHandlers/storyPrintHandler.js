9f46ce8ccf2d252c2cddee2d6ae0f3e2f60fc384
khaos119 <khaos119@gmail.com>
2024-11-13 12:06:33 -0700

    Update storyPrintHandler.js

--- src/eventsHandlers/storyPrintHandler.js
+++ src/eventsHandlers/storyPrintHandler.js
@@ -1,3 +1,14 @@
+/*
+*****
+This example code can be used as a starting point for integrating with an external print service. 
+When the event is recieved from Composer, it contains a story ID. 
+Using this ID, get the full ANS from Draft API, then format the data as needed
+and send it off to the print service. 
+Since all services differ, this example code does not include formatting, 
+but provides a way for you to quickly get up and running.
+*****
+*/
+
 const axios = require('axios');
 
 const apiHost = process.env.API_HOST;
